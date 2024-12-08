5b15f22a6211471828a8dd41d5ec6decd632e1c1
LizPlzArt <liz.please.art@gmail.com>
2024-12-08 19:43:34 +0200

    it is running

--- Number nightmare/src/NumberNightmare.java
+++ Number nightmare/src/NumberNightmare.java
@@ -14,7 +14,7 @@ public class NumberNightmare {
     //a function that checks if the users input is in valid number format, returns -1 if invalid
     public static boolean userInputValidator(String userInput)
     {
-        return userInput.matches("[0-9A-Ga-g]+b+[0-9]");
+        return userInput.matches("[0-9A-F]+b+[0-9]");
     }
 
     //a function that splits the string to number and base and returns an array of those values
@@ -31,7 +31,7 @@ public class NumberNightmare {
         dict.put("C", 12);
         dict.put("D", 13);
         dict.put("E", 14);
-        dict.put("G", 15);
+        dict.put("F", 15);
 
         String userInput = getUserInput();
         if(!userInputValidator(userInput))
@@ -42,11 +42,21 @@ public class NumberNightmare {
         String[] numbers = stringSplit(userInput);
         String number = numbers[0];
         int base = Integer.parseInt(numbers[1]);
+
         for(int i=0; i<number.length(); i++)
         {
-            String a = number.substring(i,i);
-            int temp = dict.get(a);
-            System.out.println(temp);
+            int currentInt;
+            String currentChar = number.substring(i,i+1);
+            if (!currentChar.matches("[0-9]"))
+            {
+               currentInt = dict.get(currentChar);
+            }
+            else
+            {
+                currentInt = Integer.parseInt(currentChar);
+            }
+
+
 
         }
 
